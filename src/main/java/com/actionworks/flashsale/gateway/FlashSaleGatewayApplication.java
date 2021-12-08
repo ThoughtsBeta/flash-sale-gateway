c7ea7cee2c7653d5183499740b9e81bf638c0af4
ThoughtsBeta <thoughts.beta@gmail.com>
2021-12-08 13:25:11 +0800

    初始化项目

---
+++ src/main/java/com/actionworks/flashsale/gateway/FlashSaleGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.actionworks.flashsale.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashSaleGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlashSaleGatewayApplication.class, args);
+	}
+
+}
